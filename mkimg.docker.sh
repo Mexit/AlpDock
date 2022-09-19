ca31b984a845d14382cf279bd873aa67b52e70e6
Mexit <25229673+Mexit@users.noreply.github.com>
2022-09-19 15:32:35 +0200

    New packages added: e2fsprogs, lvm2

--- mkimg.docker.sh
+++ mkimg.docker.sh
@@ -9,6 +9,6 @@ profile_docker() {
 	kernel_addons="xtables-addons"
 	apks="$apks \
 		docker docker-bash-completion docker-compose docker-compose-bash-completion docker-cli-compose \
-		bash-completion procps util-linux readline findutils sed coreutils sudo"
+		bash-completion procps util-linux readline findutils sed coreutils sudo e2fsprogs lvm2"
 	apkovl="genapkovl-docker.sh"
 }
